8278e3d58940a40b0af0acdbd14967529c4e4eb7
潘志清 <82391775+pzq123456@users.noreply.github.com>
2024-04-01 15:35:00 +0800

    Remove outdated content from README.md and update with new information. Add a description of the desired webworker management module.

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="rete" ref="rete"></div>
+</template>
+
+<script>
+
+
+</script>
+
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import { createEditor } from './editor'
+
+const rete = ref();
+
+onMounted(() => {
+  createEditor(rete.value)
+})
+
+
+</script>
+
+
+<style lang="css">
+body {
+  margin: 0;
+}
+.rete {
+  width: 100vw;
+  height: 100vh;
+}
+
+</style>

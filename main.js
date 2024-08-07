2b32c2e882cfbc8332dc21cd8cc5e57899519ac1
panzhiqing <1812673119@qq.com>
2024-08-07 15:05:53 +0800

    chore: Update canvas layer structure and styles

--- main.js
+++ main.js
@@ -1,34 +1,9 @@
-const UICanvas = document.getElementById('ui-layer');
-const gameCanvas = document.getElementById('game-layer');
+import { Canvas } from "./src/canvas.js";
+const test = document.getElementById('left');
 
-draw();
+const canvas = new Canvas(test);
+console.log(canvas.getControlLayer());
 
-function draw(){
-    drawGame(gameCanvas);
-    drawUI(UICanvas);
-}
-
-function drawUI(canvas){
-    // draw some random rectangles in the top and bottom of the screen
-    const ctx = canvas.getContext('2d');
-    ctx.fillStyle = 'red';
-    ctx.fillRect(0, 0, canvas.width, 50);
-    ctx.fillRect(0, canvas.height - 50, canvas.width, 50);
-
-    // draw some text in the center of the screen
-    ctx.fillStyle = 'black';
-    ctx.font = '30px Arial';
-    ctx.textAlign = 'center';
-    ctx.fillText('Hello World', canvas.width / 2, canvas.height / 2);
-
-}
-
-function drawGame(canvas){
-    // draw a circle in the center of the screen
-    const ctx = canvas.getContext('2d');
-    ctx.fillStyle = 'green';
-    ctx.beginPath();
-    ctx.arc(canvas.width / 2, canvas.height / 2, 50, 0, 2 * Math.PI);
-    ctx.fill();
-}
-
+let textCtx = canvas.getCtx("text");
+textCtx.fillStyle = "red";
+textCtx.fillRect(0, 0, 100, 100);
--- src/canvas.js
+++ src/canvas.js
@@ -1,35 +1,90 @@
+import { uuid } from './utils.js';
+
 /**
  * this class will create and maintain two canvas layers (exactally vertial in Z axis) like:
  * root\ zIndexFrom = 0 (e.g.)
- *  text Layer\ zIndex = 1
- *  game Layer\ zIndex = 2
+ *  game Layer\ zIndex = 1
+ *  text Layer\ zIndex = 2
+ *  control Layer\ zIndex = 3
  */
 export class Canvas{
     constructor(
-        width = 512, 
-        height = 512, 
         parentElement = null,
-        id = 'editor', 
+        id = 'editor',
+        width = 1024, 
+        height = 1024, 
         zIndexFrom = 0 
     ){
+        this.uuid = uuid();
         this.width = width;
         this.height = height;
         this.parentElement = parentElement;
         this.id = id;
         this.zIndexFrom = zIndexFrom;
-        this.layers = [];
+        this.root = null;
+        this.layers = null;
+
+        this.#createRoot();
         this.#createCanvas();
+        this.#appendRoot();
+    }
+
+    getCtx(name){
+        return this.layers[name].getContext('2d');
+    }
+
+    getControlLayer(){
+        return this.layers["control"];
+    }
+
+    #createRoot(){
+        this.root = document.createElement('div');
+        this.root.id = this.id +"-"+this.uuid;
+
+        let style = {
+            width: this.width + 'px',
+            height: this.height + 'px',
+            position: 'relative',
+            zIndex: this.zIndexFrom,
+            backgroundColor: 'gray',
+        }
+
+        for(let key in style){
+            this.root.style[key] = style[key];
+        }
+
+    }
+
+    #appendRoot(){
+        if(this.parentElement){
+            this.parentElement.appendChild(this.root);
+        }else{
+            document.body.appendChild(this.root);
+        }
     }
 
     #createCanvas(){
-        this.rootCanvas = document.createElement('canvas');
-        this.rootCanvas.width = this.width;
-        this.rootCanvas.height = this.height;
-        this.rootCanvas.id = this.id;
-        this.rootCanvas.style.position = 'absolute';
-        this.rootCanvas.style.zIndex = this.zIndexFrom;
-        this.rootCanvas.style.left = 0;
-        this.rootCanvas.style.top = 0;
-        this.parentElement.appendChild(this.rootCanvas);
+        let canvasLayerNames = ["game","text","control"];
+        let zIndex = this.zIndexFrom + 1;
+        this.layers = {};
+        for(let i = 0; i < canvasLayerNames.length; i++){
+            let name = canvasLayerNames[i];
+            let layer = this.#createCanvasLayer(name, zIndex);
+            this.layers[name] = layer;
+            this.root.appendChild(layer);
+            zIndex++;
+        }
+    }
+
+    #createCanvasLayer(name, z){
+        let myCanvas = document.createElement('canvas');
+        myCanvas.width = this.width;
+        myCanvas.height = this.height;
+        myCanvas.id = this.id + "-" + name + "-" + this.uuid;
+        myCanvas.style.position = 'absolute';
+        myCanvas.style.zIndex = z;
+
+        return myCanvas;
     }
 }
+
